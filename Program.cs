64c15bd1ed252eaebce67c6667f0f5a9a058e7b8
hgibbons11 <44006155+hgibbons11@users.noreply.github.com>
2019-07-06 11:36:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conversionWords = "Hello world today will be amazing";
+            string[] sentence = conversionWords.Split(' ');
+
+            
+            string translation = "";
+            string beginningLetter;
+            string wordBody;
+            string space = " ";
+            
+
+            foreach (string word in conversionWords.Split())
+            {
+                beginningLetter = word.Substring(0, 1);
+                wordBody = word.Substring(1, word.Length - 1);
+                translation = translation + wordBody + beginningLetter + "ay" + space;
+
+
+            }
+
+            Console.WriteLine(translation.ToString());
+            
+        }
+    }
+}
